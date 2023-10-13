c56bea27a5c7bb0190c02359415e7722458d1b87
Snoweve <a0976056641@gmail.com>
2023-10-14 00:22:51 +0800

    chore:SerializedDictionar

---
+++ Own-GameJam23-10-13/Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject stageOneSpawner;
+    public GameObject stageTwoSpawner;
+    public GameObject stageThreeSpawner;
+    public GameObject stageSecretSpawner;
+    public GameObject stageSecretTeleport;
+    
+    
+    
+
+}
